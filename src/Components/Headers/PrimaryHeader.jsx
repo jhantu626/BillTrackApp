b6965d46ee7787e84f32e64c0ec7465f601886f0
jhantu626 <jhantubala626@gmail.com>
2025-11-01 13:58:37 +0530

    primary header responsive

--- src/Components/Headers/PrimaryHeader.jsx
+++ src/Components/Headers/PrimaryHeader.jsx
@@ -3,6 +3,14 @@ import React, {memo} from 'react';
 import Octicons from '@react-native-vector-icons/octicons';
 import {fonts} from '../../utils/fonts';
 import {colors} from '../../utils/colors';
+import {
+  font,
+  gap,
+  heightResponsive,
+  icon,
+  padding,
+  widthResponsive,
+} from '../../utils/responsive';
 
 const PrimaryHeader = memo(() => {
   return (
@@ -27,35 +35,35 @@ const PrimaryHeader = memo(() => {
 
 const styles = StyleSheet.create({
   container: {
-    height: 60,
+    height: heightResponsive(60),
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    paddingHorizontal: 16,
+    paddingHorizontal: padding(16),
     backgroundColor: '#fff',
   },
   leftContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 10,
+    gap: gap(10),
   },
   image: {
-    width: 48,
-    height: 48,
+    width: icon(48),
+    height: icon(48),
   },
   text: {
-    fontSize: 11,
+    fontSize: font(11),
     fontFamily: fonts.onMedium,
   },
   name: {
-    fontSize: 16,
+    fontSize: font(16),
     fontFamily: fonts.onSemiBold,
     color: colors.primary,
   },
   dot: {
     position: 'absolute',
-    width: 10,
-    height: 10,
+    width: widthResponsive(10),
+    height: heightResponsive(10),
     borderRadius: 10 / 2,
     backgroundColor: colors.primary,
     right: 0,
