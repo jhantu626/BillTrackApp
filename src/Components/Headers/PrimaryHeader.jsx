e6a86cf9b63893ba5eb56f1047be91ea51719ce4
jhantu626 <jhantubala626@gmail.com>
2025-11-29 12:59:34 +0530

    feat: added the linear gradient of border of logo image

--- src/Components/Headers/PrimaryHeader.jsx
+++ src/Components/Headers/PrimaryHeader.jsx
@@ -1,5 +1,13 @@
-import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React, {memo} from 'react';
+import LinearGradient from 'react-native-linear-gradient';
 import Octicons from '@react-native-vector-icons/octicons';
 import {fonts} from '../../utils/fonts';
 import {colors} from '../../utils/colors';
@@ -13,24 +21,34 @@ import {
 } from '../../utils/responsive';
 import {useAuth, useBusiness, useUser} from '../../Contexts/AuthContext';
 import {greeting} from '../../utils/validator';
-import { API_URL } from '../../utils/config';
+import {API_URL} from '../../utils/config';
+import {useNavigation} from '@react-navigation/native';
 
 const PrimaryHeader = memo(() => {
   const name = useUser('name');
-  const businessUrl=useBusiness('logoUrl');
+  const businessUrl = useBusiness('logoUrl');
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
-      <View style={styles.leftContainer}>
-        <Image
-          source={{uri: `${API_URL}files/logo/${businessUrl}`}}
-          style={styles.image}
-          resizeMode='cover'
-        />
+      <Pressable
+        style={styles.leftContainer}
+        onPress={() => navigation.navigate('Business')}>
+        <LinearGradient
+          colors={['#FF6B6B', '#4ECDC4', '#45B7D1', '#FFA07A', '#98D8C8']}
+          start={{x: 0, y: 0}}
+          end={{x: 1, y: 1}}
+          style={styles.gradientBorder}>
+          <Image
+            source={{uri: `${API_URL}files/logo/${businessUrl}`}}
+            style={styles.image}
+            resizeMode="cover"
+          />
+        </LinearGradient>
         <View>
           <Text style={styles.text}>{greeting()}</Text>
           <Text style={styles.name}>{name}</Text>
         </View>
-      </View>
+      </Pressable>
       <TouchableOpacity style={styles.rightContainer}>
         <Octicons name="bell" size={24} />
         <View style={styles.dot} />
@@ -53,9 +71,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     gap: gap(10),
   },
-  image: {
+  gradientBorder: {
     width: icon(48),
     height: icon(48),
+    borderRadius: icon(48),
+    padding: 2.5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: icon(43),
+    height: icon(43),
+    borderRadius: icon(43),
+    backgroundColor: '#fff',
   },
   text: {
     fontSize: font(11),
