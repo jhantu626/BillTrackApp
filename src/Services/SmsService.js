d354bd5233b4ca65a741c8201839273e2b1a79b3
jhantu626 <jhantubala626@gmail.com>
2025-12-11 17:14:59 +0530

    create: added sms service

---
+++ src/Services/SmsService.js
@@ -0,0 +1,34 @@
+import {API_URL} from './../utils/config';
+class SmsService {
+  constructor() {
+    this.baseUrl = API_URL + 'sms';
+  }
+
+  async sendInvoiceSms({phone, invoiceNumber, totalAmount, businessName}) {
+    try {
+      const uri = `${this.baseUrl}/sent-invoice`;
+      const payload = {
+        token,
+        phone,
+        invoiceNumber,
+        totalAmount,
+        businessName,
+      };
+      const response = await axios.post(uri, payload, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = await response.data;
+      return data;
+    } catch (error) {
+      const data = await error.response.data;
+      throw data;
+    }
+  }
+}
+
+const smsService = new SmsService();
+
+export {smsService};
